2b298a4966e9f9684091a0eb03d2efb2051abb27
Berat Samli <bsamli@k1m34s02.42istanbul.com.tr>
2022-10-22 18:01:42 +0300

    sonhalyeter

--- KONTROLLL/duzelt/ft_strlcat.c
+++ KONTROLLL/duzelt/ft_strlcat.c
@@ -1,21 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bsamli <bsamli@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/17 17:28:15 by bsamli            #+#    #+#             */
-/*   Updated: 2022/10/17 18:53:48 by bsamli           ###   ########.fr       */
+/*   Created: 2022/10/21 14:31:05 by bsamli            #+#    #+#             */
+/*   Updated: 2022/10/22 13:13:52 by bsamli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isdigit(int c)
+size_t	ft_strlcat(char *dst, const char *src, size_t n)
 {
-	if (c > 47 && c < 58)
-		return (1);
-	else
-		return (0);
+	size_t	i;
+	size_t	j;
+
+	if (n <= (size_t)ft_strlen(dst))
+		return (n + ft_strlen(src));
+	i = ft_strlen(dst);
+	j = 0;
+	while ((i + 1 < n) && (src[j] != '\0'))
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(dst) + ft_strlen(&src[j]));
 }
